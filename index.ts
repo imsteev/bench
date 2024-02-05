81a5a904e47c8e6fb3ad0a75f00a2008c6be5a76
Stephen Chung <spchung95@gmail.com>
2024-02-04 22:40:47 -0500

    ya

--- index.ts
+++ index.ts
@@ -1,13 +1,15 @@
 import { page } from "./templates/root";
 
 const server = Bun.serve({
-  fetch(req) {
+  fetch(req, server) {
     return new Response(
       page({
-        html: `It is ${new Date()}
+        html: `<h1>Bench</h1>
         <div>
-          <a href="https://htmx.org/reference/" target="_blank">htmx</a>
-          <a href="https://bun.sh/docs" target="_blank">bun</a>
+          <ul>
+            <li><a href="https://bun.sh/docs" target="_blank">bun</a></li>
+            <li><a href="https://htmx.org/reference/" target="_blank">htmx</a></li>
+          </ul>
         </div>
         `,
         css: `html {
