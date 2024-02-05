dd63e41b16b07076fb7ba054466ad44329a0ea71
Stephen Chung <spchung95@gmail.com>
2024-02-05 18:59:32 -0500

    style

--- index.ts
+++ index.ts
@@ -1,20 +1,38 @@
 import { page } from "./templates/root";
 
 const server = Bun.serve({
+  port: process.env.PORT || 3000,
   fetch(req, server) {
     return new Response(
       page({
-        html: `<h1>Bench</h1>
-        <div>
-          <ul>
-            <li><a href="https://bun.sh/docs" target="_blank">bun</a></li>
-            <li><a href="https://htmx.org/reference/" target="_blank">htmx</a></li>
-          </ul>
+        html: `
+        <h1>My bench</h1>
+
+        <div class="code">
+          <code>
+bun create imsteev/bench
+          </code>
         </div>
         `,
         css: `html {
-          background: forestgreen;
-        }`,
+          background: #C3CED6;
+        }
+
+        h1 {
+          color: forestgreen;
+        }
+
+        code {
+          background: #36454F;
+          padding: 1rem;
+          border-radius: 5px;
+        }
+
+        .code {
+          display: inline-block;
+          margin-top: 2rem;
+        }
+        `,
       }),
       {
         headers: { "Content-Type": "text/html" },
